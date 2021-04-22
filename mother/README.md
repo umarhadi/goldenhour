8e286a51ebd7238c6f5f726f76f2a064d316ab06
Umar Hadi Siswanto <hi@umarhadi.xyz>
2021-04-23 00:28:35 +0800

    feat: add new music:)

---
+++ mother/README.md
@@ -0,0 +1,17 @@
+# musician
+
+Music by [Porter Robinson - Musician](https://music.youtube.com/watch?v=tm-yh5w-P4U&feature=share). Please buy this song on [iTunes](https://music.apple.com/id/album/musician/1550626757?i=1550626763&i=1550626763) if you like it!
+
+[![Screenshot](https://ik.imagekit.io/umarhadi/goldenhour/Screen_Shot_2021-04-11_at_23.44.56_9HyysKReK.png)](https://musician.gh.umarhadi.xyz)
+
+<p align="center">
+  https://musician.gh.umarhadi.xyz
+</p>
+
+---
+
+<div align="center">
+
+  Powered by [Svelte](https://svelte.dev) hosted on [Vercel](https://vercel.com)
+  
+</div>
---
+++ mother/src/App.svelte
@@ -0,0 +1,87 @@
+<script>
+  import { onMount } from 'svelte'
+
+  import Analyser from './Analyser.svelte'
+
+  let lyrics = []
+  let isShowCredit = false
+  let totalDuration = 4 * 60 - 18
+  let currentTime = 0
+  let determinedIndex = 0
+  let songTitle = 'Porter Robinson - Mother'
+
+  function constructLyrics(rawLyrics) {
+    const parsedLyrics = rawLyrics.split('\n')
+    const tsRegex = /\[(\d+):(\d+)]/g
+
+    for (let i = 0; i < parsedLyrics.length; i++) {
+      const line = parsedLyrics[i]
+      const ts = tsRegex.exec(line)
+
+      if (ts) {
+        const text = line.replace(tsRegex, '').trim()
+
+        if (text) {
+          const minutes = ts[1] * 60 * 1000
+          const seconds = ts[2] * 1000
+
+          lyrics.push({
+            ts: minutes + seconds,
+            text,
+          })
+        }
+      }
+    }
+  }
+
+  onMount(() => {
+    window
+      .fetch('Porter Robinson - Mother.txt')
+      .then((res) => res.text())
+      .then((res) => {
+        constructLyrics(res)
+      })
+
+    setTimeout(() => {
+      document.body.style.opacity = 1
+    }, 1000)
+  })
+
+  function updateCurrentTime(time) {
+    currentTime = Math.floor(time * 1000)
+
+    checkLyrics(currentTime)
+  }
+
+  function checkLyrics(time) {
+    determinedIndex = lyrics.findIndex(({ ts }) => time <= ts)
+  }
+
+  function showCredit() {
+    document.getElementsByTagName('canvas')[0].style.filter = 'blur(1px)'
+
+    setTimeout(() => {
+      isShowCredit = true
+    }, 1000)
+  }
+</script>
+
+<div>
+  <div class="o-lyrics">
+    {#if lyrics.length > 0 && determinedIndex !== -1}
+      <h4>{lyrics[determinedIndex].text}</h4>
+    {/if}
+
+    {#if isShowCredit}
+      <p>
+        <span>Song by {songTitle}</span>
+        <a href="https://github.com/umarhadi/goldenhour/tree/master/mother">@umarhadi</a>
+      </p>
+    {/if}
+  </div>
+  <Analyser
+    audio="Porter Robinson - Mother.mp3"
+    {totalDuration}
+    {updateCurrentTime}
+    {showCredit} />
+</div>
